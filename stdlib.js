b4fc306fc6b8a5215b4921d58d40a92b1e53fc7b
Marc Nieper-Wißkirchen <marc@nieper-wisskirchen.de>
2014-02-08 11:57:33 +0100

    Refactor scheme objects

--- stdlib.js
+++ stdlib.js
@@ -1,8 +1,5 @@
 /* TODO
 * Add a number of constants, e.g.
-- rapid.exitProcedure
-- rapid.schemeBoolean.true
-- rapid.schemeBoolean.false
 */
 
 'use strict';
@@ -26,40 +23,7 @@ rapid.inherits = function inherits(childCtor, parentCtor) {
   });
 };
 
-rapid.SchemeObject = function SchemeObject() {
-};
-
-rapid.SchemeObject.prototype.toBoolean = function toBoolean() {
-  return true;
-};
-
-rapid.SchemeBoolean = function SchemeBoolean(boolean) {
-  this._boolean = boolean;
-};
-rapid.inherits(rapid.SchemeBoolean, rapid.SchemeObject);
-
-rapid.SchemeBoolean.prototype.toBoolean = function toBoolean() {
-  return this._boolean;
-};
-
-rapid.SchemeBoolean.true = new rapid.SchemeBoolean(true);
-rapid.SchemeBoolean.false = new rapid.SchemeBoolean(false);
-
-rapid.SchemeString = function (string) {
-  rapid.SchemeObject.call(this);
-  this._string = string;
-};
-rapid.inherits(rapid.SchemeString, rapid.SchemeObject);
-
-rapid.SchemeString.prototype.toString = function toString() {
-  return this._string;
-};
-
-rapid.Procedure = function Procedure(code) {
-  rapid.SchemeObject.call(this);
-  this.code = code;
-};
-rapid.inherits(rapid.Procedure, rapid.SchemeObject);
+importScripts("schemeobject.js");
 
 function display(obj) {
   // This is more string-write
