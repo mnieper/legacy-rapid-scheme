f245ea4b1ed6f81de5031c50b3ee3841452495b6
Marc Nieper-Wißkirchen <marc@nieper-wisskirchen.de>
2016-02-29 11:08:56 +0100

    WIP

--- examples/ck.scm
+++ examples/ck.scm
@@ -4,14 +4,12 @@
 ;;   but need a later call
 
 
-#;(import (scheme base)
-	(scheme case-lambda)
+(import (scheme base)
 	(scheme write))
-(import (rapid primitive))
+;(import (rapid primitive))
 
-(define-syntax aux (syntax-rules ... ()))
-
-;; use ck-aux
+(define-syntax :call (syntax-rules ... ()))
+(define-syntax :prepare (syntax-rules ... ()))
 
 (define-syntax ck
   (syntax-rules ... (quote)
@@ -19,7 +17,7 @@
     ((ck (((op ...) ea ...) . s) 'v)
      (ck s "arg" (op ... 'v) ea ...))
     ((ck s "arg" (op va ...))
-     (op s va ...))
+     (op :call s va ...))
     ((ck s "arg" (op ...) 'v ea1 ...)
      (ck s "arg" (op ... 'v) ea1 ...))
     ((ck s "arg" (op ...) ea ea1 ...)
@@ -27,24 +25,33 @@
     ((ck s (op ea ...))
      (ck s "arg" (op) ea ...))))
 
-(define-syntax ck*
-  (syntax-rules ... (quote)
-    ((ck* () 'v) v)
-    ((ck* (((op ...) ea ...) . s) 'v)
-     (ck* s "arg" (op ... 'v) ea ...))
-    ((ck* s "arg" (op va ...))
-     (op aux s va ...))
-    ((ck* s "arg" (op ...) 'v ea1 ...)
-     (ck* s "arg" (op ... 'v) ea1 ...))
-    ((ck* s "arg" (op ...) ea ea1 ...)
-     (ck* (((op ...) ea1 ...) . s) ea))
-    ((ck* s (op ea ...))
-     (ck* s "arg" (op) ea ...))))
+(define-syntax define-macro
+  (syntax-rules ... ()
+    ((define-macro op ellipsis (literal ...)
+       (pattern template)
+       ...)
+     (begin
+       (define-syntax define-macro1
+	 (syntax-rules ...1 (ellipsis op quote)
+	   ((_ o e (l ...1) () ((p t) ...1))
+	    (define-syntax op
+	      (syntax-rules e (l ...1 quote aux)
+		((op :call s . p) (ck s t))
+		...1
+		;; TODO :prepare
+		((op . args) (ck () (op . args))))))
+	   ((_ o e l* (((op . p) t) . pt*) qu*)
+	    (_ o e l* pt* qu* (p t) ()))
+	   ((_ o e l* pt* (qu ...1) (() t) p)
+	    (_ o e l* pt* (qu ...1 (p t))))
+	   ((_ o e l* pt* qu* ((x . p) t) (y ...1))
+	    (_ o e l* pt* qu* (p t) (y ...1 x)))))
+       (define-macro1 op ellipsis (literal ...  (pattern template) ... ()))))))
 
 ;; TODO: Rename a few things
 ;; FIXME: Need to pass a couple of things to the inner macro
 ;;        Chibi does it right; don't test with gosh
-(define-syntax define-macro
+#;(define-syntax define-macro
   (syntax-rules ... (case-lambda)
     ((define-macro op ellipsis (literal ...)
        (case-lambda 
@@ -72,18 +79,15 @@
 	      (f ::: 'x)))))     
        (define-macro-aux () ((%formals %expression) ...))))))
 
-(define-macro $cons ... ()
-  (case-lambda
-   ((h t) '(h . t))))
+(define-macro m-cons ... ()
+  ((m-cons 'h 't) '(h . t)))
 
-(define-macro $append ... ()
-  (case-lambda
-   ((() l2) 'l2)
-   (((h . t) l2) ($cons 'h ($append 't 'l2)))))
+(define-macro m-append ... ()
+  ((m-append '() 'l2) 'l2)
+  ((m-append '(h . t) 'l2) (m-cons 'h (m-append 't 'l2))))
 
-(define-macro $quote ... ()
-  (case-lambda
-   ((x) ''x)))
+(define-macro m-quote ... ()
+  ((m-quote 'x) ''x))
 
 ;; The macro below won't work that simply...
 ;; because it has to thread everything to kt and kf
@@ -97,5 +101,5 @@
 		     ((test _) kf))))
        (test y)))))
 
-(display ($quote ($append '(1 2 3) '(4 5))))
+(display (m-quote (m-append '(1 2 3) '(4 5))))
 (newline)
